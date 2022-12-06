322d3cf5f10a151a20f5e408cfe760b6aa222a6e
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-12-06 11:48:39 -0500

    ejercicio5

--- sistema.js
+++ sistema.js
@@ -1,14 +1,19 @@
 addEventListener("DOMContentLoaded", (e)=> {
-    let formulario = document.querySelector('#form1')
+    var chec1 = document.getElementById("check3")
+    var chec2 = document.getElementById("check4")
+    var chec3 = document.getElementById("check2")
+    var chekear = document.getElementById("check1");
+    var list = document.getElementById("list");
 
-    formulario.addEventListener('submit', (e) => {
-        e.preventDefault()
-        let datos = Object.fromEntries(new FormData(e.target))
-        let gCelsius = datos.grados
-        let operacion = (gCelsius*9/5)+32
-        grados1.innerHTML = `${operacion} Grados Farenheit en New York..`
-        hola.innerHTML = operacion
+    chekear.addEventListener("click", (e)=>
+    {
+        if (chekear.checked && chec1.checked && chec2.checked && chec3.checked) {
+            list.innerHTML = "Haz sido agilizado tu dia. ";
+        } else{
+            list.innerHTML = "Tienes tareas por terminar.";
+        }
+
     })
 
-    })
+})
 
