db79fa9b9ae3482b23829055c4e9a2903f304f09
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2022-12-06 11:45:24 -0500

    ejercicio4

--- sistema.js
+++ sistema.js
@@ -1,23 +1,14 @@
 addEventListener("DOMContentLoaded", (e)=> {
     let formulario = document.querySelector('#form1')
-    let formulario2 = document.querySelector('#form2')
 
     formulario.addEventListener('submit', (e) => {
         e.preventDefault()
         let datos = Object.fromEntries(new FormData(e.target))
-        let vDolar = parseFloat(4800)
-        let input = datos.dolar
-        let operacion = vDolar*input
-        dolares.innerHTML = `${operacion} Pesos`
-
-    })
-    formulario2.addEventListener('submit', (e) => {
-        e.preventDefault()
-        let datos = Object.fromEntries(new FormData(e.target))
-        let vPeso = 0.00021
-        let input = datos.peso
-        let operacion = input*vPeso
-        pesos.innerHTML = `${operacion} Dolares`
+        let gCelsius = datos.grados
+        let operacion = (gCelsius*9/5)+32
+        grados1.innerHTML = `${operacion} Grados Farenheit en New York..`
+        hola.innerHTML = operacion
     })
 
     })
+
